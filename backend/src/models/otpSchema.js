fb63aca1c14f7c60fd9a61e4af500cc9bfac1c82
Aarav Vaish <aaravvaish2004@gmail.com>
2025-12-04 20:39:16 +0530

    error fixed , otp request email was set to unique

--- backend/src/models/otpSchema.js
+++ backend/src/models/otpSchema.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const otpSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true},
     otp: { type: String, required: true },
     createdAt: { type: Date, default: Date.now, expires: 300 }, // OTP expires in 5 minutes
 }, { timestamps: true });
