40772593b7e8208212a1bb78603c52967337229d
Aarav Vaish <aaravvaish2004@gmail.com>
2025-12-09 01:33:44 +0530

    route handled

--- backend/src/routes/globalpostroutes.js
+++ backend/src/routes/globalpostroutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getglobalposts, createglobalpost , likeGlobalPost } from '../controllers/globalpostcontroller.js';
+import { getglobalfeed, createGlobalPost , likeGlobalPost } from '../controllers/globalpostcontroller.js';
 import {authmiddleware} from '../middleware/authmiddleware.js';
+import { get } from 'mongoose';
 
 const router = express.Router();
 
-router.get('/getglobalposts', authmiddleware, getglobalposts);
-router.post('/createglobalpost', authmiddleware, createglobalpost);
-router.post('/likeglobalpost/:id/like', authmiddleware, likeGlobalPost);
+router.get('/getglobalposts', authmiddleware, getglobalfeed);
+router.post('/createglobal', authmiddleware, createGlobalPost);
+router.post('/:id/like', authmiddleware, likeGlobalPost);
 
 export default router;
