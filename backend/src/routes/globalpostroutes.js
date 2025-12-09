c2f841a66bbdd4ef43e5753af5511bfc3940a9aa
Aarav Vaish <aaravvaish2004@gmail.com>
2025-12-09 12:22:21 +0530

    cloudinary implemented but testing yet

--- backend/src/routes/globalpostroutes.js
+++ backend/src/routes/globalpostroutes.js
@@ -2,11 +2,11 @@ import express from 'express';
 import { getglobalfeed, createGlobalPost , likeGlobalPost } from '../controllers/globalpostcontroller.js';
 import {authmiddleware} from '../middleware/authmiddleware.js';
 import { get } from 'mongoose';
-
+import {upload} from "../middleware/upload.js";
 const router = express.Router();
 
 router.get('/getglobalposts', authmiddleware, getglobalfeed);
-router.post('/createglobal', authmiddleware, createGlobalPost);
+router.post('/createglobalposts', authmiddleware, upload.array("images", 4), createGlobalPost);
 router.post('/:id/like', authmiddleware, likeGlobalPost);
 
 export default router;
