f9ca6f6d85e697db05991bdfb2599440d1157256
Aarav Vaish <aaravvaish2004@gmail.com>
2025-12-16 23:26:47 +0530

    adding get message route

--- backend/src/controllers/communitypostcontroller.js
+++ backend/src/controllers/communitypostcontroller.js
@@ -2,5 +2,12 @@ import {CommunityMessage} from "../models/communitymessageschema.js"
 
 
 export const getComunityMessages = async( req , res)=>{
-    
+    const {community_id} = req.params;
+    try{
+
+    }
+    catch(e){
+        
+    }
+
 }
