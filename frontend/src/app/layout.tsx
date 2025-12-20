09620237dadf22641fe5b05c6c24eae5defb9516
sumit-616 <sumit616794@gmail.com>
2025-12-20 14:01:17 +0530

    frontend app layout.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { ThemeToggle } from "@/components/theme/theme-toggle";
+import { ThemeInit } from "@/components/theme/themeInit";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +26,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
         <div className="fixed top-4 right-4 z-50">
+          <ThemeInit/>
           <ThemeToggle />
         </div>
         <Toaster />
