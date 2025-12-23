b36b801b89f18fde03049c733fe11aba18b95ae6
Aarav Vaish <aaravvaish2004@gmail.com>
2025-12-23 20:24:53 +0530

    community like contoller

--- backend/src/controllers/communitypostcontroller.js
+++ backend/src/controllers/communitypostcontroller.js
@@ -1,6 +1,5 @@
 import CommunityMessage from "../models/communitymessageschema.js"
 
-
 export const getCommunityMessages = async( req , res)=>{
     const {community_id} = req.params;
     try{
@@ -12,4 +11,43 @@ console.log("error in getting community messages ", e);
 
     }
 
+}
+
+export const likeCommunityMessage = async(req , res)=>{
+    const {id}= req.params;
+    const {user_id} = req.body;
+    try{
+        const message = await CommunityMessage.findById(id);
+        if(!message) return res.status(404).json({message: "Message not found"});
+         const hasLiked = message.likedBy.includes(user_id);
+
+    if (hasLiked) {
+        message.likes--;
+        message.likedBy = message.likedBy.filter(uid => uid !== user_id);
+        
+    } else {
+        message.likes++;
+        message.likedBy.push(user_id);
+    }
+    
+    await message.save();
+    return res.json({ 
+        message: hasLiked ? "Unliked" : "Liked",
+        likes: message.likes 
+      });
+}
+    catch(e){
+console.log("error in liking community message ", e);
+
+    }
+}
+
+export const commentCommunityMessage= async(req, res)=>{
+    try{
+    const {id}= req.params;
+    const { user_id, randomName, comment} = req.body;
+    }
+    catch(e){
+
+    }
 }
